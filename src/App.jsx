51c58bd14272b92fbb426c387c344ddf48d095b4
raiji-kun <huot.edouard@gmail.com>
2025-04-08 11:35:41 +0900

    f010

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { useState } from "react"
 function App(){
 let [nom, setNom] = useState("")
 
-const handleclik = () =>{
+const handleclik = (e) =>{
+  e.preventDefault(); 
+  
   if(nom.trim() === ""){
    
     alert("Please fill out this field");
     return ;
   }
+  setNom("")
   alert("bonjour "+ nom)
- console.log(nom)
 }
 
 
@@ -18,9 +20,11 @@ const handleclik = () =>{
 return (<div>
   <h1> bienvenue chez nous </h1>
   <h2>connectez vous !</h2>
-
+    <form onSubmit={handleclik} >
     <input value={nom} onChange={(e) => setNom(e.target.value)} type="text" placeholder="entrez votre prenom" required/>
-  <button onClick={handleclik} >accedez a votre espace</button>
+    <input type="submit"  value="accedez a votre espace" />
+    </form>
+  
   </div>
 )}
 
